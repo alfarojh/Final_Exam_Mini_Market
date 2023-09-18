7678fa889f0cc79f3823665b4623f09d426e80b1
alfarojh <alfarous.jh@gmail.com>
2023-09-18 14:51:30 +0700

    Create project mini market wgs

---
+++ src/main/java/com/example/mini_market_wgs/MiniMarketWgsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_market_wgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMarketWgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMarketWgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_market_wgs/MiniMarketWgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_market_wgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMarketWgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
